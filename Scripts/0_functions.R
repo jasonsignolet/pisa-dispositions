0a06bf8158113ced24fa1ac40342c51a5539ad24
jasonsignolet <jasonsignolet@gmail.com>
2016-05-15 16:22:42 +0930

    Write ETL file (partial)

---
+++ Scripts/0_functions.R
@@ -0,0 +1,39 @@
+#########################################
+## FUNCTIONS
+#########################################
+
+
+
+#########################################
+## Use the Pisa2012 dictionary to set missing values to NA
+#########################################
+
+missingPISA <- function(input, dictionary){
+  require(data.table)
+  
+  variables_input <- colnames(input)
+  variables_dict <- unique(dictionary$variable)
+  
+  variables <- intersect(variables_input, variables_dict)
+  
+  for(i in variables){
+    tryCatch(
+      {
+        missing <- 
+          dictionary[variable == (i) & Label %in% c("N/A", "Missing", "Invalid"), value]
+        }, 
+      error = function(e){return(missing <- NA)
+      }
+    )
+    
+    
+    if(is.na(missing[1])){
+      next
+    }
+    
+    input[eval(as.name(i)) %in% as.numeric(missing), eval(as.name(i)) := NA]
+  }
+  
+  return(input)
+  
+}
